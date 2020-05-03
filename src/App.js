7daca6ae23135d33f0aaacb0dc29334949b4a0d2
Alisa Chang <laptite@gmail.com>
2020-05-02 21:05:03 -0700

    Update signout to clear user

--- src/App.js
+++ src/App.js
@@ -26,24 +26,26 @@ const particlesOptions = {
   }
 }
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  boxes: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
 class App extends Component {
   
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      boxes: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -113,9 +115,9 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({ isSignedIn: false })
+      this.setState(initialState);
     } else if (route === 'home') {
-      this.setState({ isSignedIn: true })
+      this.setState({ isSignedIn: true });
     } 
     this.setState({ route: route });
   }
